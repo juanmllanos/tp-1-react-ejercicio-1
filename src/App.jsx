68876c1e84f9317829232c816e1a7948dae9f57b
juanmllanos <juan_m@hotmail.com.ar>
2023-06-01 21:31:15 -0300

    ejercicio terminado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HolaMundo from './HolaMundo';
+
+function App() {
+  return (
+    <>
+      <HolaMundo></HolaMundo>
+    </>
+  );
+}
+
+export default App;
---
+++ src/HolaMundo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HolaMundo = () => {
+  return <h1 className='text-center display-1 my-5'>Hola Mundo</h1>;
+};
+
+export default HolaMundo;
